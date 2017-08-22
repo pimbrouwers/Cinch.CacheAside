fef20af33cb8df38ed5df7ea99d8e73ab75b552d
Pim Brouwers <pbrouwers@nhlpa.com>
2017-08-22 15:58:54 -0400

    hello world

---
+++ Cinch.CacheAside/AsyncLazy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinch.CacheAside
+{
+    public class AsyncLazy<T> : Lazy<Task<T>>
+    {
+        public AsyncLazy(Func<T> valueFactory) :
+            base(() => Task.Factory.StartNew(valueFactory))
+        { }
+        public AsyncLazy(Func<Task<T>> taskFactory) :
+            base(() => Task.Factory.StartNew(() => taskFactory()).Unwrap())
+        { }
+    }
+}
---
+++ Cinch.CacheAside/Extensions/IMemoryCacheExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Threading.Tasks;
+
+namespace Cinch.CacheAside
+{    
+    public static class IMemoryCacheExtensions
+    {
+        public static T GetOrAdd<T>(this IMemoryCache cache, string key, int seconds, Func<T> factory, bool isAbsolute = true)
+        {
+            return cache.GetOrCreate<T>(key, entry => new Lazy<T>(() =>
+            {
+                if (isAbsolute)
+                    entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(seconds);
+                else
+                    entry.SlidingExpiration = TimeSpan.FromSeconds(seconds);
+
+                return factory.Invoke();
+            }).Value);
+        }
+
+        public static Task<T> GetOrAddAsync<T>(this IMemoryCache cache, string key, int seconds, Func<Task<T>> taskFactory, bool isAbsolute = true)
+        {
+            return cache.GetOrCreateAsync<T>(key, async entry => await new AsyncLazy<T>(async () =>
+            { 
+                if (isAbsolute)
+                    entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(seconds);
+                else
+                    entry.SlidingExpiration = TimeSpan.FromSeconds(seconds);
+
+                return await taskFactory.Invoke();
+            }).Value);
+        }
+    }
+}
